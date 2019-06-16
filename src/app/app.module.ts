65640f9ed219e953b582235f4b0e7ae081c75149
TuyVo <vthtuy2@gmail.com>
2019-06-16 14:20:28 -0500

    test the commit permission & init searchcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { AuthInterceptor } from './auth.interceptor';
 import { HomeComponent } from './home/home.component';
 import { routes } from './routes';
+import { SearchcomponentComponent } from './searchcomponent/searchcomponent.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SearchcomponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/searchcomponent/searchcomponent.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchcomponent',
+  template: `
+    <p>
+      searchcomponent works!
+    </p>
+  `,
+  styles: []
+})
+export class SearchcomponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
