a690ea8fde8a6d56391923253372233665f12ed0
TuyVo <vthtuy2@gmail.com>
2019-06-17 18:23:29 -0500

    get signed in account

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../services/profile.service'; 
 import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -12,11 +13,13 @@ export class ProfileComponent implements OnInit {
   
   sub : Subscription;
   
-  user={email: 'hmm@gmail.com', name: "Handsome Mohammed"}
+  user={email : ''}
 
-  constructor(private http : HttpClient, private profileService : ProfileService) { }
+  constructor(private http : HttpClient, private profileService : ProfileService, private authService : AuthService) { }
 
-  ngOnInit() { 
+  ngOnInit() {
+      this.user= this.authService.user;
+      console.log('this.user.email', this.authService.user);
       this.sub = this.profileService.getProfile(this.user.email).subscribe( (res) => {
       this.user = res;
     });
