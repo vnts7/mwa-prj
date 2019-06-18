509ee6eac473df9c5c46186e19e59ef54d78b537
Minh Phuong Tran <phuongtm1911@gmail.com>
2019-06-18 13:48:33 -0500

    Update about-us.component.ts

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: 'app-about-us',
+  templateUrl: './about-us.component.html',
+  styleUrls: ['./about-us.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutUsComponent implements OnInit {
 
   constructor() { }
 
