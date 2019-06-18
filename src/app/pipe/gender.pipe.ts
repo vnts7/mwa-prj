10b4bcbc362730fe50f69279b453b20c092d7975
TuyVo <vthtuy2@gmail.com>
2019-06-18 13:54:10 -0500

    update gender pipe

--- src/app/pipe/gender.pipe.ts
+++ src/app/pipe/gender.pipe.ts
@@ -5,6 +5,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 export class GenderPipe implements PipeTransform {
     transform(value : Boolean, arg? : any) {
+        if (value == null) {return ' (Input Gender so that I can help you)';}
         return value ? 'Female' : 'Male'
     }
 }
